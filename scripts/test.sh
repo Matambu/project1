e0df5800ccb2e24998531c63002007b01faa227e
Matambu <markomatambu@outlook.com>
2022-05-23 08:33:19 +0000

    m

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 #!/bin/sh
+cd
 sudo apt update
 sudo apt install python3 python3-pip python3-venv -y
 python3 -m venv venv
 . venv/bin/activate
+cd project1/Service1/application
 pip3 install pytest
 pip3 install -r requirements.txt
+cd ..
+cd ..
 
 cd Service1/
 pip3 install -r requirements.txt
